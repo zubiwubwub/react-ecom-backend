07d161b468f543cbd474bae39a14b896d81750db
Ruslan Zubenko <zubenkoruslan@gmail.com>
2021-02-12 00:19:38 +0200

    styled Nav

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ec9f6f8a1142522e71e6";
+/******/ 	var hotCurrentHash = "b42abe328884abb79b13";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
