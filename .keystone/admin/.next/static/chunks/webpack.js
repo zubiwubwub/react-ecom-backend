e9d9595661623c7833b958b728a98feee567ff2d
Ruslan Zubenko <zubenkoruslan@gmail.com>
2021-02-17 12:44:53 +0200

    Added items to cart with addToCart

--- .keystone/admin/.next/static/chunks/webpack.js
+++ .keystone/admin/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "630a65d0a8cd235f03f5";
+/******/ 	var hotCurrentHash = "1e8ff3d2c7f5fbb84c40";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- mutations/index.ts
+++ mutations/index.ts
@@ -1,17 +1,16 @@
 import { graphQLSchemaExtension } from '@keystone-next/keystone/schema';
+import addToCart from './addToCart';
 
 // make fake graphql taggrd template literal
 export const extendGraphqlSchema = graphQLSchemaExtension({
   typeDefs: `
     type Mutation {
-      addToCart(productID: ID): CartItem
+      addToCart(productId: ID): CartItem
     }
   `,
   resolvers: {
     Mutation: {
-      addToCart() {
-        console.log('Add to cart');
-      },
-    }
-  }
+      addToCart,
+    },
+  },
 });
